13ae87eb8db841f8f71d8e1729d822741ed104ab
Sattha Puangput <sattha.puangput@gmail.com>
2018-02-02 14:54:16 +0700

    Roll back to original code

--- src/com/mighty16/json/annotations/GsonAnnotations.java
+++ src/com/mighty16/json/annotations/GsonAnnotations.java
@@ -3,9 +3,9 @@ package com.mighty16.json.annotations;
 
 public class GsonAnnotations implements AnnotationGenerator {
 
-
     @Override
     public String getSerializeName(String jsonKey) {
-        return "@Expose\n@SerializedName(\"" + jsonKey + "\")";
+        return "@SerializedName(\"" + jsonKey + "\")";
     }
+
 }
--- src/com/mighty16/json/models/FieldModel.java
+++ src/com/mighty16/json/models/FieldModel.java
@@ -23,6 +23,6 @@ public class FieldModel {
     }
 
     public boolean needsSerializesName() {
-        return true;
+        return !name.equals(jsonName);
     }
 }
